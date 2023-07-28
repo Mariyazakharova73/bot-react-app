3dbafcf2e2a538bda5a307e1d585327fe8d47e15
Mariya Zakharova <zakharovamaria73@yandex.ru>
2023-07-28 14:49:36 +0400

    feat: request

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import "./ProductList.css";
 import ProductItem from "./../ProductItem/ProductItem";
 import { useTelegram } from "./../../hooks/useTelegam";
@@ -14,16 +14,15 @@ const products = [
   { id: "8", title: "Куртка 5", price: 12000, description: "Зеленого цвета, теплая" },
 ];
 
-
 const getTotalPrice = (items = []) => {
-return items.reduce((acc, item)=> {
-return acc += item.price
-}, 0)
-}
+  return items.reduce((acc, item) => {
+    return (acc += item.price);
+  }, 0);
+};
 
 const ProductList = () => {
   const [addedItems, setAddedItems] = useState([]);
-  const { tg } = useTelegram();
+  const { tg, queryId } = useTelegram();
 
   const onAdd = (product) => {
     const alreadyAdded = addedItems.find((item) => item.id === product.id);
@@ -40,9 +39,32 @@ const ProductList = () => {
       tg.MainButton.hide();
     } else {
       tg.MainButton.show();
-      tg.MainButton.setParams({text:`Купить ${getTotalPrice(newItems)}`})
+      tg.MainButton.setParams({ text: `Купить ${getTotalPrice(newItems)}` });
     }
   };
+
+  const onSendData = useCallback(() => {
+    const data = {
+      products: addedItems,
+      totalPrice: getTotalPrice(addedItems),
+      queryId: queryId
+    };
+
+    fetch("http://localhost:8000", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringyfy(data),
+    });
+  }, [addedItems]);
+
+  useEffect(() => {
+    tg.onEvent("mainButtonClicked", onSendData);
+    return () => {
+      tg.offEvent("mainButtonClicked", onSendData);
+    };
+  }, [onSendData, tg]);
 
   return (
     <div className="list">
