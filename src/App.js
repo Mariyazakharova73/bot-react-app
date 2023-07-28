bb4022371a4f6822e9ec4a56be6f01b4c0f3f296
Mariya Zakharova <zakharovamaria73@yandex.ru>
2023-07-28 09:59:31 +0400

    feat: start

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import "./App.css";
-
-
+import Header from "./components/Header/Header";
 
 function App() {
   const tg = window.Telegram.WebApp;
@@ -12,7 +11,7 @@ function App() {
 
   return (
     <div className="App">
-      
+      <Header />
     </div>
   );
 }
